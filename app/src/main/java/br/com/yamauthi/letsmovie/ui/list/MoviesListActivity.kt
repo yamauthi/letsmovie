9837c190f133f84c0d07e7292fbc2f3212c354f9
Hugo Yamauthi Silva <hugo.yamauthi@gmail.com>
2018-08-23 23:41:08 -0300

    Add initial project structure

---
+++ app/src/main/java/br/com/yamauthi/letsmovie/ui/list/MoviesListActivity.kt
@@ -0,0 +1,13 @@
+package br.com.yamauthi.letsmovie.ui.list
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.yamauthi.letsmovie.R
+
+class MoviesListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies_list)
+    }
+}
